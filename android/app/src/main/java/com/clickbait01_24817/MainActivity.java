e9798245553010f9eb0a6a0173a606d8ba034da7
Crowdbotics <team@crowdbotics.com>
2021-03-03 00:56:41 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/clickbait01_24817/MainActivity.java
@@ -0,0 +1,15 @@
+package com.clickbait01_24817;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+    /**
+    * Returns the name of the main component registered from JavaScript. This is used to schedule
+    * rendering of the component.
+    */
+    @Override
+    protected String getMainComponentName() {
+        return "clickbait01_24817";
+    }
+}
---
+++ src/config/app.js
@@ -0,0 +1,5 @@
+export const appConfig = {
+  // todo add library to handle env variables
+  emailAuthAPIEndPoint: "https://clickbait01-24817.botics.co",
+  defaultTimeout: 5000
+};
